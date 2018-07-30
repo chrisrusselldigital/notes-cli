2c61a01a2468077aadb06f1490c60174aaab8adf
Chris Russell <chris.john.russell@gmail.com>
2018-07-30 20:05:04 +0100

    Add config for simplecov

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Makers' Goal: CLI Note app
 
 A command-line interface app, demonstrating some Ruby chops.
+
+## Testing
+
+TDD - `RSpec`
+Code style - `rubocop`
+Code coverage - `SimpleCov`
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,11 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'simplecov'
+require 'rspec/simplecov'
+SimpleCov.start
+SimpleCov.minimum_coverage 90
+RSpec::SimpleCov.start
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
