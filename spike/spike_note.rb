bb0e35e447034949d76b86b12d1beb568c2ffb5e
Chris Russell <chris.john.russell@gmail.com>
2018-08-30 15:41:01 +0100

    Add view feature to spike

--- spike/spike_note.rb
+++ spike/spike_note.rb
@@ -22,7 +22,7 @@ class Notes
         note_id: (@note_id += 1)
       }
       @notebook << @note
-      puts "note stored"
+      puts "note (id: #{@note_id}) stored"
       puts @notebook
     end
   end
@@ -35,34 +35,29 @@ All: to see all the notes currently stored.
 Exit: to exit the notes app"
   end
 
-  def write_note(content)
-    @note = {
-      content: "content",
-      time: "time"
-    }
-    @note[:content] = content
-    time = Time.now.strftime('%d-%m-%Y')
-    @note[:time] = time
-    @notebook << @note
-  end
-
   def list_of_notes
     @notebook
   end
 
   def all_notes_header
-    puts "Content || Created"
+    puts ["NoteId || Note || Created"]
   end
 
   def all_notes
     @notebook.each do |item|
-      puts "#{item[:content]} || #{item[:time]}"
+      puts "#{item[:note_id]} || #{item[:note]} || #{item[:time]}"
     end
   end
 
-  def print
-    puts all_notes_header
-    puts all_notes
+  def view(id)
+    @notebook.select do |note|
+      note[:note_id] == (id)
+    end
+  end
+
+  def all
+    print all_notes_header
+    print all_notes; nil
   end
 
 end
